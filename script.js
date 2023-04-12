cb838972eb223a24bc3b4c0a45adf2ef7eb9e488
Victor de Souto <victor.souto@hotmail.com>
2023-04-12 18:46:54 -0300

    feat: save paciente

--- script.js
+++ script.js
@@ -4,6 +4,44 @@ const fecharModalBtnElement = document.querySelector('#close-btn')
 const formElement = document.querySelector('.form-usuario')
 const modalElement = document.querySelector('#modal-cadastrar-user')
 
+//  ----- INÍCIO  ----- Funçoes para manipular API
+
+async function createPaciente(paciente) {
+  return fetch('http://localhost:3000/pacientes', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(paciente)
+  })
+}
+
+//  ----- FIM  -------- Funçoes para manipular API
+
+const botaoSalvarTag = document.querySelector('#salvar-btn')
+
+async function handleSavePaciente(){
+  const nomeInputTag = document.querySelector('#nome');
+  const emailInputTag = document.querySelector('#email');
+  const senhaInputTag = document.querySelector('#senha');
+
+  const paciente = {
+    nome: nomeInputTag.value,
+    email: emailInputTag.value,
+    senha: senhaInputTag.value,
+  }
+
+  await createPaciente(paciente)
+  toggleModal()
+}
+
+botaoSalvarTag.addEventListener('click', handleSavePaciente)
+
+//  ----- INÍCIO  ----- Funçoes para manipular Formulário
+
+//  ----- FIM  -------- Funçoes para manipular Formulário
+
 function toggleModal() {
   // capturar o modal
   const modalTemDisplayFlex = modalElement.style.display === "flex"
@@ -15,23 +53,23 @@ function toggleModal() {
   }
 }
 
-function clicouNoBtnSalvar(evento) {
-  evento.preventDefault()
+// function clicouNoBtnSalvar(evento) {
+//   evento.preventDefault()
 
-  // Capturar os valores dos inputs
-  const nomeInputElement = document.querySelector('#nome');
-  const emailInputElement = document.querySelector('#email');
-  const senhaInputElement = document.querySelector('#senha');
+//   // Capturar os valores dos inputs
+//   const nomeInputElement = document.querySelector('#nome');
+//   const emailInputElement = document.querySelector('#email');
+//   const senhaInputElement = document.querySelector('#senha');
 
-  const usuario = {
-    nome: nomeInputElement.value,
-    email: emailInputElement.value,
-    senha: senhaInputElement.value,
-  }
+//   const usuario = {
+//     nome: nomeInputElement.value,
+//     email: emailInputElement.value,
+//     senha: senhaInputElement.value,
+//   }
 
-  toggleModal()
-}
+//   toggleModal()
+// }
 
 abrirModalBtnElement.addEventListener('click', toggleModal)
 fecharModalBtnElement.addEventListener('click', toggleModal)
-formElement.addEventListener('submit', clicouNoBtnSalvar)
+// formElement.addEventListener('submit', clicouNoBtnSalvar)
